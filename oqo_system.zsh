7fe4c8e7b37b16e3069cf0e26d088d2547c40913
Indieterminacy <indieterminacy@nortonspeal.com>
2019-01-02 22:12:17 +0100

    Added some inactive plugins

--- oqo_system.zsh
+++ oqo_system.zsh
@@ -1,3 +1,20 @@
 #!/usr/bin/zsh
 
 zplug "grml/zsh-lovers"
+
+############
+# oqo_zplug
+############
+# Update Zplug binary
+# zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+
+############
+# Seems to be an issue with [[
+# pieces
+# Can manage gist file just like other plugins
+# zplug "b4b4r07/79ee61f7c140c63d2786", \
+    # from:gist, \
+    # as:command, \
+    # use:get_last_pane_path.sh
+
+# Seems to be a problem loading this script
